89acfe6adeb5e654f52f7a9157eb1fec8d91a7b4
maceskerm1 <70300813+maceskerm1@users.noreply.github.com>
2020-11-29 23:24:52 -0500

    Eh

--- client.py
+++ client.py
@@ -1,28 +1,28 @@
 import socket
 
 client_socket = socket.socket()
-port = 12345
-client_socket.connect(('serverip',port))
+port = 2001
+client_socket.connect(('192.168.1.109',port))
 
 
 recv_msg = client_socket.recv(1024)
-print recv_msg
+print(recv_msg)
 
 #makes the user
-send_msg = raw_input("Enter Username:")
-client_socket.send(send_msg)
+send_msg = input("Enter Username:")
+client_socket.send(send_msg.encode())
 
 
 
 #send messages to users
 while True:
     recv_msg = client_socket.recv(1024)
-    print recv_msg
+    print (recv_msg)
     print("Enter as @user:text")
-    send_msg = raw_input("Send Message")
+    send_msg = input("Send Message")
     if send_msg == 'Close':
         break;
     else:
-        client_socket.send(send_msg)
+        client_socket.send(send_msg.encode())
 
 client_socket.close()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket,select
 
-port = 5005
+port = 2001
 socket_list = []
 users = {}
 
@@ -8,7 +8,7 @@ users = {}
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(('',port))
-server_socket.listen(5)
+server_socket.listen(2001)
 socket_list.append(server_socket)
 
 
@@ -18,14 +18,14 @@ while True:
         if sock == server_socket:
             connect, addr = server_socket.accept()
             socket_list.append(connect)
-            connect.send("You are connected at:" + str(addr))
+            connect.send("You are connected at:" + addr.encode())
         else:
             try:
                 data = sock.recv(2048)
                 if data.startswith("#"):
                     users[data[1:].lower()]=connect
-                    print "User " + data[1:] +" connected."
-                    connect.send("Your username is: "+str(data[1:]))
+                    print( "User " + data[1:] +" connected.")
+                    connect.send("Your username is: "+data[1:].encode())
                 elif data.startswith("@"):
                     users[data[1:data.index(':')].lower()].send(data[data.index(':')+1:])
             except:

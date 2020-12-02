179358ea8224ba6b53cb024154b1e0666d60085e
Jeffrey S <70288418+jeff00363@users.noreply.github.com>
2020-12-01 23:02:36 -0500

    updates

--- main.py
+++ main.py
@@ -20,67 +20,130 @@ from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.scrollview import ScrollView
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 
 
-class TitleFunction(GridLayout):
+class ConnectionInfo(GridLayout):
     def __init__(self, **kwargs):
-        self.appName = Label(text="CSC265 Messaging App", width=Window.size[0] * 0.8, size_hint_x=None, multiline=False)
-        self.settings = Button(text="Settings", width=Window.size[0] * 0.1, size_hint_y=None, multiline=False)
-        self.returns = Button(text="Return", width=Window.size[0] * 0.1, size_hint_y=None, multiline=False)
-        self.settings.bind(on_press=self.settings)
-        self.returns.bind(on_press=MainMenu())
+        super().__init__(**kwargs)
+        self.cols = 2
 
-        topline = GridLayout(cols=1)
-        topline.add_widget(self.settings)
-        topline.add_widget(self.appName)
-        topline.add_widget(self.returns)
-        self.add_widget(topline)
+        # user sets the input for the ip address
+        self.add_widget(Label(text="IP ADDRESS: "))
+        self.ipAddress = TextInput(multiline=False)
+        self.add_widget(self.ipAddress)
+
+        # user sets the input for the port address
+        self.add_widget(Label(text="PORT NUMBER: "))
+        self.portNumber = TextInput(multiline=False)
+        self.add_widget(self.portNumber)
+
+        # user sets the input for the username
+        self.add_widget(Label(text="NAME: "))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+
+        # user sets the input button to join a chat and runs the script
+        self.add_widget(Label(text="JOIN CHAT: "))
+        self.connectBTN(Button("CONNECT"))
+        self.join.bind(on_press=self.click)
+        self.add_widget(self.connectBTN)
+
+    def click(self, instance):  # This function will save the data from the input to variables
+        Clock.schedule_once(self.connect, 1)
+
+        ipa = self.ipAddress.text
+        portn = self.portNumber.text
+        user = self.username.text
+
+        # print(f"{user} is connecting to {ipa} at port {portn} ")
+        with open("connections.txt", "w") as f:
+            f.write(f"{user},{ipa},{portn}")
+        info = f"Connecting {user} to {ipa}:{portn} "
+        dataPage.info_page.update_info(info)
 
 
-class MainMenu(GridLayout):
+class dataPage(GridLayout):
     def __init__(self, **kwargs):
-        super(MainMenu, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+        self.cols = 1
 
-        self.cols = 2  # The Number of columns in grid is set
+        self.data = Label(halign="center", valign="middle", font_size=30)
+        self.add_widget(self.message)
 
-        self.add_widget(TitleFunction())
-        # Sets the apps title box
-
-        self.messagePreview = Label(text="Messages")  # Will load file for the messages saved locally
-        self.add_widget(self.messagePreview)
+    def updateData(self, textData):
+        self.data.text = textData
 
 
-class ChatMenu(GridLayout):
+class messageDisplay(ScrollView):
     def __init__(self, **kwargs):
-        super(ChatMenu, self).__init__(**kwargs)
+        super().__init__(**kwargs)
 
-        self.cols = 3  # The Number of columns in grid is set
-        self.rows = 3
+        self.layout = GridLayout(cols=1, size_hint_y=None)
+        self.add_widget(self.layout)
 
-        topline = self.add_widget(TitleFunction())
-        # Sets the apps title box and app functions
+        self.messagesOld = Label(size_hint_y=None, markup=True)
+        self.layout.add_widget(self.messagesOld)
 
-        self.messagePreview = Label()  # Will load a txt file of saved files
-        self.add_widget(self.messagePreview)
-        # Load the message Preview
+    def updateMessages(self, messages):
+        self.history(self, messages)
+        self.layout.height = self.messagesOld.texture_size[1] + 10
+        self.history.height = self.messagesOld.texture_size[1]
 
-        self.newMessage = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False)
-        self.send = Button(text="Send")
-        self.send.bind(on_press=self.sendMessage)
-        messageline = GridLayout(cols=3)
-        messageline.add_widget(self.newMessage)
-        messageline.add_widget(self.send)
-        self.add_widget(messageline)
-        # shows the user text input and send button
+        self.scroll_to(self.scroll_to_point)
+
+class ChatPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.rows = 2
+
+        self.history = Label(height=Window.size[1] * 0.9, size_hint_y=None)
+        self.add_widget(self.history)
+
+        self.message = TextInput(width=Window.size[0] * 0.8, size_hint_x=None, multiline=False)
+        self.sendBTN = Button(text="Send")
+        self.sendBTN.bind(on_press=self.sendBTN)
+
+        inputLine = GridLayout(cols=2)
+        inputLine.add_widget(self.message)
+        inputLine.add_widget(self.sendBTN)
+        self.add_widget(inputLine)
 
 
-class Homepage(App):  # runs the homepage
+class HomepageLogin(App):  # runs the homepage and login
     def build(self):
-        return Label(text = 'MainMenu')
+        self.display = ScreenManager()
+
+        self.connect = ConnectionInfo()
+        mains = Screen(name="Connect Data")
+        mains.add_widget(self.connect)
+        self.display.add_widget(mains)
+
+        self.dataprep = dataPage()
+        mains = Screen(name="Data Processing")
+        mains.add_widget(self.dataprep)
+        self.display.add_widget(mains)
+        return self.display
+
+
+
+    def create_chat_page(self):
+        self.chatPage = ChatPage()
+        mains = Screen(name='Chat')
+        mains.add_widget(self.chatPage)
+        self.display.add_widget(mains)
+
+
+def errors(message):
+    mains = Screen(name="ERROR")
+    mains.add_widget(Label("Error"))
+
 
 if __name__ == "__main__":
-    Homepage().run()
+    txttotxt = HomepageLogin()
+    txttotxt.run()

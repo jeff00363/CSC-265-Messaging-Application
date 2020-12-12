146eb8cedda149734012be55f375393087576d59
AryanB-23bit <50510280+AryanB-23bit@users.noreply.github.com>
2020-12-12 16:03:37 -0500

    EVERYTHING DONE

--- 265 Messaging Application/265-Msg-App/Final Client1.py
+++ 265 Messaging Application/265-Msg-App/Final Client1.py
@@ -6,12 +6,13 @@ import sys, socket
 
 format = 'utf-8'
 
-listKey = [b'a3uo8T5xtcfRIVbWuMmkIDjAiRnFff8ZoBVOagf16xg=']
-f = Fernet(listKey[0])
+listKey = [b'a3uo8T5xtcfRIVbWuMmkIDjAiRnFff8ZoBVOagf16xg='] # encrypt key
+f = Fernet(listKey[0]) # fernet object
 
 userIP = socket.gethostbyname(socket.gethostname())
 PORT = 9997
 
+# encrypt and decrypt func
 def encrypt(msg):
     eCr = f.encrypt(msg)
     return eCr
@@ -22,6 +23,7 @@ def decrypt(msg):
     return decoded
 
 
+# making client and connecting to the socket also setting up the GUI fields
 class makeClient(object):
     def __init__(self):
         self.chatUser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -35,18 +37,22 @@ class makeClient(object):
         self.chatGfx.userMsgInput.clicked.connect(self.sendMsg)
         self.FromUIConn = logIn.Ui_Form()
         self.FromUIConn.setupUi(self.connUi)
-        self.FromUIConn.userNameInput.clicked.connect(self.handshake)
+        self.FromUIConn.userNameInput.clicked.connect(self.recvFromServ)
         self.mainChat.setGeometry(QtCore.QRect(1000, 550, 650, 500))
         self.mainChat.show()
 
+# msg is sent to the GUI to show on the client side
     def msgDisplay(self, msg):
         self.chatGfx.textWindow.append(msg)
 
+# connection with server through userIP and PORT and sending the userName to server form initial sending and adding
+    # name to list of clients connected with the server
     def connToServ(self, userName):
         self.chatUser.connect((userIP, PORT))
         self.chatUser.send(userName.encode(format))
         print("--Connected to server--")
 
+# sending an encrypted message
     def sendMsg(self):
         nickname = self.FromUIConn.nameTextEdit.toPlainText()
         msgUser = (str(nickname) + ":-" + (str(self.chatGfx.userTxt.toPlainText())))
@@ -56,7 +62,8 @@ class makeClient(object):
         self.chatUser.send(encryptedMsg)
         self.chatGfx.userTxt.clear()
 
-    def handshake(self):
+# recv the msg from serv through the Thread that gets the message from the server and display it on the UI
+    def recvFromServ(self):
         userName = self.FromUIConn.nameTextEdit.toPlainText()
         self.connToServ(userName)
         self.connUi.setHidden(True)
@@ -66,7 +73,7 @@ class makeClient(object):
         self.recv.start()
         print("--Thread started--")
 
-
+# using the function of threading and QTcore. the thread recvs the msg and sends it to the UI and we decrypt it before it sends it anywhere
 class Thread(QtCore.QThread):
     sig = QtCore.pyqtSignal(str)
     def __init__(self, client_socket):
@@ -80,6 +87,8 @@ class Thread(QtCore.QThread):
         deCr = decrypt(msg)
         self.sig.emit(deCr)
 
+
+# Chatapp is made and started and userCleint is init and started
 
 chat = QtWidgets.QApplication(sys.argv)
 chatApp = makeClient()
--- 265 Messaging Application/265-Msg-App/Final Server.py
+++ 265 Messaging Application/265-Msg-App/Final Server.py
@@ -1,9 +1,9 @@
 import socket, threading
 from cryptography.fernet import Fernet
 
+# encoding format
 format = 'utf-8'
 
-inRoom = {}
 port = 9997
 hostIP = socket.gethostbyname(socket.gethostname())
 
@@ -11,21 +11,12 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((hostIP, port))
 server.listen(20)
 
-listKey = [b'a3uo8T5xtcfRIVbWuMmkIDjAiRnFff8ZoBVOagf16xg=']
-f = Fernet(listKey[0])
 
-def encrypt(msg):
-    eCr = f.encrypt(msg)
-    print(eCr)
-    return eCr
-def decrypt(msg):
-    dCr = f.decrypt(msg)
-    decoded = dCr.decode(format)
-    print(dCr)
-    return decoded
+inRoom = {}  # a dict to store username and server.accept() value as key value pair so we can get the addr just by the user name
 
-
-def handshake():
+# initial contact with the server, and server gets the user name which is added to the dict with userName being the key
+# thread also starts so more than one client cant join and send and/or recv msgs
+def handshakeAndMsgDoorWay():
     print("--Server running on \n IP : %s  \n Port : %s" % (hostIP, port))
 
     while True:
@@ -38,12 +29,16 @@ def handshake():
 
         thread.start()
 
+# send msg func sends messages to every one except the person it through sending it form userName and get addr from dict
 def sendMsg(msg, otherClientUserName):
     for userName in inRoom:
         if userName != otherClientUserName:
             user_msg = msg
             inRoom[userName].send(user_msg.encode(format))
 
+
+# this is where we can get our messages from the client. Server cant read them because we dont decrypt on serverside.
+# It also has a failsafe so if the client bugs out the server does crash. It just disconnects the client
 def msgPipeline(client_conn, userName):
     print('--Waiting for a response\n')
     while True:
@@ -63,4 +58,4 @@ def msgPipeline(client_conn, userName):
             break
 
 
-handshake()
+handshakeAndMsgDoorWay()

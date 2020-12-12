dc84c431ffabaf9841df9275d614d48151ee8d48
AryanB-23bit <50510280+AryanB-23bit@users.noreply.github.com>
2020-12-11 21:52:56 -0500

    Added new file

--- 265 Messaging Application/logIn.py
+++ 265 Messaging Application/logIn.py
@@ -4,12 +4,12 @@ class Ui_Form(object):
     def setupUi(self, userEnter):
         userEnter.setObjectName("userEnter")
         userEnter.resize(324, 336)
-        self.hostTextEdit = QtWidgets.QPlainTextEdit(userEnter)
-        self.hostTextEdit.setGeometry(QtCore.QRect(160, 40, 145, 30))
-        self.hostTextEdit.setObjectName("hostTextEdit")
-        self.portTextEdit = QtWidgets.QPlainTextEdit(userEnter)
-        self.portTextEdit.setGeometry(QtCore.QRect(160, 80, 145, 31))
-        self.portTextEdit.setObjectName("portTextEdit")
+        # self.hostTextEdit = QtWidgets.QPlainTextEdit(userEnter)
+        # self.hostTextEdit.setGeometry(QtCore.QRect(160, 40, 145, 30))
+        # self.hostTextEdit.setObjectName("hostTextEdit")
+        # self.portTextEdit = QtWidgets.QPlainTextEdit(userEnter)
+        # self.portTextEdit.setGeometry(QtCore.QRect(160, 80, 145, 31))
+        # self.portTextEdit.setObjectName("portTextEdit")
         self.nameTextEdit = QtWidgets.QPlainTextEdit(userEnter)
         self.nameTextEdit.setGeometry(QtCore.QRect(160, 120, 145, 31))
         self.nameTextEdit.setObjectName("nameTextEdit")
@@ -42,7 +42,7 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
         Form.setWindowTitle(_translate("userEnter", "userEnter"))
-        self.label.setText(_translate("userEnter", "Hostname"))
-        self.label_2.setText(_translate("userEnter", "Port"))
+        # self.label.setText(_translate("userEnter", "Hostname"))
+        # self.label_2.setText(_translate("userEnter", "Port"))
         self.label_3.setText(_translate("userEnter", "Name"))
         self.pushButton.setText(_translate("userEnter", "Connect"))
